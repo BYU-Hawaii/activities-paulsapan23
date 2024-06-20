40b0ad3265afa9e5f5d9e516b2baeeb4b575462b
Paul Sapan <paulsapan23@go.byuh.edu>
2024-06-19 23:35:20 -1000

    update

--- Activity-18/scripts/script.js
+++ Activity-18/scripts/script.js
@@ -9,7 +9,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
     var emailValid = /^[^@]+@\w+(\.\w+)+\w$/.test(email); // Simple email pattern check
     var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password); // Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters
-    var contactValid = /^\+(?:[0-9] ?){6,14}[0-9]$/.test(contact); // Phone number validation
+    var contactValid = /^(?:\(\d{3}\)|\d{3}-)\d{3}-\d{4}$/.test(contact); // US phone number validation
 
     document.getElementById('usernameFeedback').style.display = usernameValid ? 'none' : 'block';
     document.getElementById('emailFeedback').style.display = emailValid ? 'none' : 'block';
